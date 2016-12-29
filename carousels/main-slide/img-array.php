0595b46f1af1e0a69dafa2aca1bd7d6da47f481b
kitsu3 <ksc3.14159@gmail.com>
2016-12-28 17:00:16 -0800

    Update img-array.php

--- carousels/main-slide/img-array.php
+++ carousels/main-slide/img-array.php
@@ -2,21 +2,15 @@
 
 	$images = array(
 		array(
-			"name" => "decorative-glass-1.jpg",
-			"alt" => "Decorative glass panel lights by Sandy"
+			"name" => "rippling-flower-reflection.jpg",
+			"alt" => "Reflection of geraniums in rippling water",
 		),
 		array(
-			"name" => "fairy-ornaments.jpg",
-			"alt" => "Assorted Christmas tree ornaments by Sandy"
+			"name" => "student-library.jpg",
+			"alt" => "ABC High School library",
+			"short" => "ABC Library",
+			"caption" => "ABC High School student in our new library"
 		),
-		array(
-			"name" => "jar-lights-2.jpg",
-			"alt" => "Artistic jar lights by Sandy"
-		),
-		array(
-			"name" => "cloth-angels.jpg",
-			"alt" => "Cloth angel ornaments by Sandy"
-		)
 	);
 
 ?>
